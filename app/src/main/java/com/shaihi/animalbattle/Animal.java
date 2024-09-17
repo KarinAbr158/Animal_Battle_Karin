e90950fe904e2b81e6db0c5ec04aa2a136a8ba11
shaihi <shaihi@icloud.com>
2024-09-17 23:20:51 +0300

    Added the base Class for the implementation

---
+++ app/src/main/java/com/shaihi/animalbattle/Animal.java
@@ -0,0 +1,16 @@
+package com.shaihi.animalbattle;
+
+public abstract class Animal {
+    protected int strength;
+
+    public Animal(int strength) {
+        this.strength = strength;
+    }
+
+    // Abstract method to be overridden in subclasses
+    public abstract String battle(Animal other);
+
+    public int getStrength() {
+        return strength;
+    }
+}
